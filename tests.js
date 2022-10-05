223ffe3058ac3080e9a887bd9951290fc163b5f7
Sushmana <sushmanag73@gmail.com>
2022-10-05 12:29:50 +0530

    modified sqeuclidean Distance

--- tests.js
+++ tests.js
@@ -1,3 +1,3 @@
-const {euclideanDistance, sqeuclideanDistance} = require("./index");
-console.log(euclideanDistance([-7, -4],[17, 6.5]))
-console.log( sqeuclideanDistance([-7, -4],[17, 6.5]))
+const { euclideanDistance, sqeuclideanDistance } = require("./index");
+console.log(euclideanDistance([-7, -4], [17, 6.5]))
+console.log(sqeuclideanDistance([-7, -4], [17, 6.5]))
